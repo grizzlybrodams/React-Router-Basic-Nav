ec885c66bb20e4182fe4dd382e620239826a230d
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2019-10-02 20:01:20 -0600

    finished the assignment

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 const Navigation = () => {
   return (
@@ -6,13 +7,16 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+           <a href="/">Home</a> 
+          <Link to= "/"></Link>
         </div>
         <div>
-          <a href="">About</a>
+          <a href="/About">About</a>
+          <Link to="/About"></Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <a href="/Contact">Contact</a>
+          <Link to="/Contact"></Link>
         </div>
       </div>
     </div>
